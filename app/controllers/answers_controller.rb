8339724c26e54a4cef56200ccb1679b1acd6fbc4
Thomas Feng <email": null>
2017-05-18 11:51:40 -0400

    created model/migration/routes for answers

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,32 @@
+post '/questions/:question_id/answers' do
+  answer = current_user.answers.create(params[:answer])
+  redirect '/questions/:question_id'
+end
+
+get '/questions/:question_id/answers/:id/edit' do
+  @answer = Answer.find_by(id: params[:id])
+  if @answer.user_id == current_user.id
+    erb :"/answers/edit"
+  else
+    redirect "/questions/#{params[:question_id]}"
+  end
+end
+
+put '/questions/:question_id/answers/:id' do
+  answer = Answer.find_by(id: params[:id])
+  if answer.user_id == current_user.id
+    answer.update(param[:answer])
+  else
+    redirect "/questions/#{params[:question_id]}"
+  end
+end
+
+delete '/questions/:question_id/answers/:id' do
+  answer = Answer.find_by(id: params[:id])
+  if answer.user_id == current_user.id
+    answer.destroy
+    redirect "/questions/#{params[:question_id]}"
+  else
+    redirect "/questions/#{params[:question_id]}"
+  end
+end
