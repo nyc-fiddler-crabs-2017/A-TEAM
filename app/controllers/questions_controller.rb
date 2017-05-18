9c169c923e99b07dfebec9ea0e485494ba1598d5
STEVEN AGUILAR <email": null>
2017-05-18 15:47:53 -0400

    create routes for delete, update and new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,3 +7,45 @@ end
 get '/questions/new' do
   erb :'questions/new'
 end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+
+  erb :'questions/show'
+end
+
+get '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+
+  erb :'questions/edit'
+end
+
+put '/questions/:id' do
+  @question = Question.find(params[:id])
+
+  @question.assign_attributes(params[:question])
+
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'questions/edit'
+  end
+end
+
+delete '/questions/id' do
+  @question = Question.find(params[:id])
+
+  @question.destroy
+
+  redirect '/questions'
+end
