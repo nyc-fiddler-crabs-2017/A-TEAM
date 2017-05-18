eff1f1c5d696f679ab86fb9fcb1b9aca7b80dffd
Jason <jason.89.wang@gmail.com>
2017-05-18 12:10:24 -0400

    refactor helper method
    You tha man jason

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,6 @@
 helpers do
   def current_user
-    @current_user ||= session[:user_id] &&
-    User.find_by_id(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def is_logged_in?
