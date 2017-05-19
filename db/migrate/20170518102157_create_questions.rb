88d22bc7c2d7ab46143587994640947dffaa27e6
Jason Wang <jason.89.wang@gmail.com>
2017-05-19 10:27:01 -0400

    added models and migrations for votes and comments

--- db/migrate/20170518102157_create_questions.rb
+++ db/migrate/20170518102157_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :title
-      t.string :text
+      t.string :title, null: false
+      t.string :text, null: false
       t.references :user,  foreign_key: true
 
       t.timestamps
--- db/migrate/20170518102212_create_votes.rb
+++ db/migrate/20170518102212_create_votes.rb
@@ -1,4 +1,12 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+    create_table :votes do |t|
+      t.string  :total
+      t.references :user, foreign_key: true
+      t.integer :voteable_id, null: false
+      t.string  :voteable_type, null: false
+
+     t.timestamps null: false
+   end
   end
 end
