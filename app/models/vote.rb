a3da4a3d907fa6c9c751fc8b071275e122fd84ab
Jason Wang <jason.89.wang@gmail.com>
2017-05-19 11:13:00 -0400

    prep to merge

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
 class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
 end
--- db/migrate/20170518102212_create_votes.rb
+++ db/migrate/20170518102212_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.string  :total
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, null: false
       t.integer :voteable_id, null: false
       t.string  :voteable_type, null: false
 
